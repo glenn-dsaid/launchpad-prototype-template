bee15ea48dc76a6388a45ded5fd7f6f11ecb41dc
glenn-dsaid <71322438+glenn-dsaid@users.noreply.github.com>
2023-04-24 15:52:25 +0800

    Added feedback form for response

--- prototype-template.py
+++ prototype-template.py
@@ -6,6 +6,7 @@ import streamlit as st
 st.set_page_config(page_title="LaunchPad Prototype",
                    page_icon="images/launchpad-icon.png", layout="centered")
 
+
 # Display logo image
 launchpad_icon = "images/launchpad-icon.png"
 st.image(launchpad_icon, width=100)
@@ -27,6 +28,50 @@ st.divider()
 
 # --------------------- CONTENT AREA START (ADD YOUR CODE AFTER HERE) ------------------
 
+
+# Create function to handle feedback submission
+
+def submit_feedback(feedback_text, feedback_rating):
+    if not feedback_text:
+        st.error("Please provide feedback before submitting.")
+    else:
+        st.success("Thank you for your feedback!")
+
+# Define Streamlit app
+
+
+def app():
+    st.title("Feedback Section")
+
+    # Prompt user for feedback
+    feedback_text = st.text_area("Please provide your feedback here:")
+
+    # Allow user to rate the response
+    feedback_rating = st.selectbox("Did you find the response helpful?", [
+                                   "Thumbs Up", "Thumbs Down"])
+
+    # Create container to hold feedback elements
+    with st.container():
+        st.write("Please provide your feedback on the response below.")
+        st.write("Your feedback will help us improve our service.")
+
+        # Add thumbs up/down buttons
+        col1, col2 = st.beta_columns(2)
+        with col1:
+            if st.button("👍 Thumbs Up"):
+                feedback_rating = "Thumbs Up"
+        with col2:
+            if st.button("👎 Thumbs Down"):
+                feedback_rating = "Thumbs Down"
+
+        # Add comment box
+        feedback_comment = st.text_area("Additional comments (optional):")
+
+        # Add submit button
+        if st.button("Submit Feedback"):
+            submit_feedback(feedback_comment, feedback_rating)
+
+
 # Text area example. To provide tooltip and placeholder example where applicable to guide user.
 prompt = st.text_area("Text area",
                       height=200, help="Provide a simple tooltip explanation to help user", placeholder="Give an example or instruction to guide user")
@@ -36,7 +81,8 @@ if st.button("Submit"):
     if prompt != "":
         with st.spinner('Generating response...'):
             st.divider()
-            st.subheader("The response is:")
+            st.subheader("The response is2:")
+            app()
         st.success(prompt)
     else:
         st.error("You must input a prompt to get started")
