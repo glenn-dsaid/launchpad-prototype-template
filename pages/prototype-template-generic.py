8ab5879e56129b906e6ec7b4b2dc31ce69fd053b
glenn-dsaid <71322438+glenn-dsaid@users.noreply.github.com>
2023-07-26 10:21:16 +0800

    updated to include new disclaimer text

--- pages/prototype-template-generic.py
+++ pages/prototype-template-generic.py
@@ -55,7 +55,7 @@ st.title("Application Title")  # USER TO EDIT TITLE
 
 # Display disclaimer message (Edit where necessary)
 st.warning("""**Please read the following before proceeding:**
-        *• This application is currently in **Beta version** and supports data classified up to **Restricted and Sensitive (Normal)**. 
+        *• This application is an external application currently in **Beta version** and supports data classified up to **Restricted and Sensitive (Normal)**. 
         • **DO NOT** put Personal Identifiable Information (PII) within this application.
         • This application is under active testing and your prompts will be logged to improve your experience.
         • By using this application, you acknowledge that you recognise the possibility of AI generating inaccurate or wrong responses, and you take full responsibility over how you use the generated output.*""")
